69c6550d5c74d717b9c5e38cf8007b8800d07c2b
Bilal Asry <bilal.asry@elev.ga.ntig.se>
2024-11-12 12:50:16 +0100

    näsan över E första steg

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Hej");
+
+Console.WriteLine("Välj ett tal mellan 10 och 0");
+while (true)
+{
+    string valj = Console.ReadLine();
+    int pick;
+    int.TryParse(valj, out pick);
+
+
+    if (pick <= 6 && pick >= 3)
+    {
+        Console.WriteLine("Hello World!");
+
+        break;
+    }
+
+    else if (pick > 6 || pick < 3)
+    {
+        Console.WriteLine("försök igen!!");
+
+    }
+}
+
+Console.ReadLine();
+
+
+if (6 >= 3) {
+    Console.WriteLine("Hello world");
+}
